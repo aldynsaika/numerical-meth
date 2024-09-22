20b1f492be7bee5c60481b1a9072f6425b98cbd5
aldynsaika <a.mongush3@g.nsu.ru>
2024-09-22 23:55:50 +0700

    add 1 task

--- 2 лаба/2_1.py
+++ 2 лаба/2_1.py
@@ -11,8 +11,7 @@ def f(x):
     return (((g*m)/x) * (1 - math.exp(-(x/m)*t)) - v)
 
 
-#модифицированный regula falsi
-def mod_regula_falsi(xl, xu, es, imax):
+def mod_regula_falsi(xl, xu, es=10**-15, imax=10**16):
     iter = 0
     xr = xl
     ea = 0
@@ -51,66 +50,71 @@ def mod_regula_falsi(xl, xu, es, imax):
 
         if ea < es or iter >= imax:
             break
-    return xr, iter, ea
+    return xr, iter
 
-def dichotomy(a, b, e):
+
+def dichotomy(a, b, e=10**-15):
     fa, fb = f(a), f(b)
-    if fa * fb >= 0:
-        raise ValueError("Функция должна иметь разные знаки на концах интервала")
-
-    while (b - a) / 2 > tol:
+    i = 0
+    while (b - a) / 2 > e:
+        i += 1
         c = (a + b) / 2
         fc = f(c)
         if fc == 0:
-            return c
+            return c, i
         elif fa * fc < 0:
             b = c
         else:
             a = c
             fa = fc
-    return (a + b) / 2
+    return (a + b) / 2, i
 
-#regula falsi
-# def regula_falsi(b, a):
-#     while b - a > e:
-#         c = (b - a) / 2
-#         if (func1(a) * func1(c)) > 0:
-#             a = c
-#         if (func1(a) * func1(c)) < 0:
-#             b = c
-#         if (func1(a) * func1(c)) == 0:
-#             ans = c
-#             break
-#         ans = a
-#     return ans
+
+def regula_falsi(a, b, e=10**-15):
+    fa, fb = f(a), f(b)
+    i = 0
+    while abs(b - a) > e:
+        i += 1
+        c = b - fb * (b - a) / (fb - fa)
+        fc = f(c)
+        if abs(fc) < e:
+            return c, i
+        elif fa * fc < 0:
+            b, fb = c, fc
+        else:
+            a, fa = c, fc
+    return c, i
 
 
 c = 0
-a = 1
-b = 100
-e = 0.00001
+a = 10**-9
+b = 200
 ans = 0
 
+x, iterations = dichotomy(a, b)
+print(f"Метод дихотомии\nКорень: {x} Итерации:{iterations}\n")
 
-# ans = regula_falsi(b, a)
+x, iterations = regula_falsi(a, b)
+print(f"Метод regula falsi\nКорень: {x} Итерации:{iterations}\n")
 
-x, iterations, error = mod_regula_falsi(a, b, e, 1000)
-print(f"Модифицированный метод\nКорень: {x} Итерации:{iterations} Ошибка: {error}")
+x, iterations = mod_regula_falsi(a, b)
+print(f"Модифицированный метод regula falsi\nКорень: {x} Итерации:{iterations}")
 
 
 
 
-# n = 1000
-# x = np.linspace(0, n, n)
-# y1 = []
-# for i in range(1, n+1):
-#         curr = func1(c)
-#         y1.append(curr)
-#
-# y1 = np.array(y1)[:n]
-#
-# data = [go.Scatter(x=x, y=y1, name='func')
-#         ]
-# fig = go.Figure(data=data)
-# fig.show()
-# fig.write_image('task2.png', scale = 1)
+
+n = 1000
+x = np.linspace(0, n, n)
+y1 = []
+for i in range(1, n+1):
+        curr = f(i)
+        y1.append(curr)
+
+y1 = np.array(y1)[:n]
+
+data = [go.Scatter(x=x, y=y1, name='func')
+        ]
+fig = go.Figure(data=data)
+fig.show()
+fig.write_image('task2.png', scale = 1)
